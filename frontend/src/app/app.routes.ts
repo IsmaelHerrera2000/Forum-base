64dd8e512c69969cd1bfed7e496d6b3e257953f5
IsmaelHerrera2000 <ismherram@alum.us.es>
2024-10-12 23:48:58 +0200

    feat: add welcome

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component'; 
+import { WelcomeComponent } from './welcome/welcome.component';
 
 export const routes: Routes = [
-  { path: '', component: UserComponent },
+  { path: '', component: WelcomeComponent },
+  { path: 'users', component: UserComponent },
 ];
 
 @NgModule({
